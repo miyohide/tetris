0dace2ae09b713b4e91938ec56e1e38340ab14bf
Hidenori Miyoshi <miyohide@gmail.com>
2021-01-20 18:48:33 +0900

    途中の行の初期値を確認

--- __tests__/js/field.test.js
+++ __tests__/js/field.test.js
@@ -10,4 +10,14 @@ describe('tileAt', () => {
     }
     expect(field.tileAt(width - 1, 0)).toBe(1)
   })
+
+  it('初期状態における途中の行の確認', () => {
+    let field = new Field(undefined)
+    let width = field.GAME_WIDTH
+    expect(field.tileAt(0, 5)).toBe(1)
+    for (let i = 1; i < width - 1; i++) {
+      expect(field.tileAt(i, 5)).toBe(0)
+    }
+    expect(field.tileAt(width - 1, 5)).toBe(1)
+  })
 })
