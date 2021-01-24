4cc4dd1c2e0d4d4b3d64870ee5be2b4f7722d39d
Hidenori Miyoshi <miyohide@gmail.com>
2021-01-24 16:27:30 +0900

    add a test when rot is 4 and shape is 0

--- __tests__/js/mini.test.js
+++ __tests__/js/mini.test.js
@@ -58,5 +58,19 @@ describe('calcBlocks', () => {
         expect(blocks[i].equals(expect_blocks[i])).toBe(true)
       }
     })
+
+    it('rotが4のとき360度回転したT字型のブロックが返ること', () => {
+      let mino = new Mino(undefined, 5, 10, 4, 0)
+      let expect_blocks = [
+        new Block(undefined, 4, 10),
+        new Block(undefined, 5, 10),
+        new Block(undefined, 5, 9),
+        new Block(undefined, 6, 10)
+      ]
+      let blocks = mino.calcBlocks()
+      for (let i = 0; i < blocks.length; i++) {
+        expect(blocks[i].equals(expect_blocks[i])).toBe(true)
+      }
+    })
   })
 })
