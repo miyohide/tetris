1d44dbe20da2fdefd81a0f653ad93dd0d10c0286
Hidenori Miyoshi <miyohide@gmail.com>
2021-01-24 16:01:49 +0900

    add a Mino.calcBlocks function test.

---
+++ __tests__/js/mini.test.js
@@ -0,0 +1,20 @@
+import { Mino } from '../../src/js/mino'
+import { Block } from '../../src/js/block'
+
+describe('calcBlocks', () => {
+  describe('shape is 0', () => {
+    it('should ', () => {
+      let mino = new Mino(undefined, 5, 10, 0, 0)
+      let expect_blocks = [
+        new Block(undefined, 4, 10),
+        new Block(undefined, 5, 10),
+        new Block(undefined, 5, 9),
+        new Block(undefined, 6, 10)
+      ]
+      let blocks = mino.calcBlocks()
+      for (let i = 0; i < blocks.length; i++) {
+        expect(blocks[i].equals(expect_blocks[i])).toBe(true)
+      }
+    })
+  })
+})
