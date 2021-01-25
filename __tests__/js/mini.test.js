83fd921cc2f7b4c9f821aab22df4ca68048aae00
Hidenori Miyoshi <miyohide@gmail.com>
2021-01-25 20:48:27 +0900

    Add a test when shape = 1

--- __tests__/js/mini.test.js
+++ __tests__/js/mini.test.js
@@ -63,4 +63,18 @@ describe('calcBlocks', () => {
       expect(blocks).toEqual(expect_blocks)
     })
   })
+
+  describe('shape is 1', () => {
+    it('逆z字型のブロックが返ること', () => {
+      let mino = new Mino(undefined, 5, 10, 0, 1)
+      let expect_blocks = [
+        new Block(undefined, 4, 9),
+        new Block(undefined, 5, 9),
+        new Block(undefined, 5, 10),
+        new Block(undefined, 6, 10)
+      ]
+      let blocks = mino.calcBlocks()
+      expect(blocks).toEqual(expect_blocks)
+    })
+  })
 })
