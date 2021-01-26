533ede4556e7aaa4e4e56c8b222a7fe11240bfcc
Hidenori Miyoshi <miyohide@gmail.com>
2021-01-26 20:51:02 +0900

    Add a test when shape = 1 and rot = 1

--- __tests__/js/mini.test.js
+++ __tests__/js/mini.test.js
@@ -65,13 +65,25 @@ describe('calcBlocks', () => {
   })
 
   describe('shape is 1', () => {
-    it('逆z字型のブロックが返ること', () => {
+    it('z字型のブロックが返ること', () => {
       let mino = new Mino(undefined, 5, 10, 0, 1)
       let expect_blocks = [
         new Block(undefined, 4, 9),
         new Block(undefined, 5, 9),
         new Block(undefined, 5, 10),
         new Block(undefined, 6, 10)
+      ]
+      let blocks = mino.calcBlocks()
+      expect(blocks).toEqual(expect_blocks)
+    })
+
+    it('rotが1のとき90度回転したz字型のブロックが返ること', () => {
+      let mino = new Mino(undefined, 5, 10, 1, 1)
+      let expect_blocks = [
+        new Block(undefined, 6, 9),
+        new Block(undefined, 6, 10),
+        new Block(undefined, 5, 10),
+        new Block(undefined, 5, 11)
       ]
       let blocks = mino.calcBlocks()
       expect(blocks).toEqual(expect_blocks)
