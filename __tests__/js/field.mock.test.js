2930fea7dbd6d53bd842b211d5131dc2695dd63d
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-06 18:46:32 +0900

    Fix test.

--- __tests__/js/field.mock.test.js
+++ __tests__/js/field.mock.test.js
@@ -5,14 +5,17 @@ jest.mock('../../src/js/block')
 
 describe('draw', () => {
   it('call Block.draw', () => {
+    const draw_mock = jest.fn()
     Block.mockImplementation(() => {
       return {
-        draw: () => {
-          console.log('call block.draw')
-        }
+        draw: draw_mock
       }
     })
     const f = new Field(undefined)
     f.draw()
+    // Fieldにて1である数のチェック。
+    // 底辺以外の両端が1+底辺が全て1。
+    // 高さが21、幅が12であるため、2（両端）x20（底辺以外の高さ）+12（底辺）=52
+    expect(draw_mock.mock.calls.length).toBe(52)
   })
 })
